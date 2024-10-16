e43015d2f8959d6fb379a6db8844f4d74dc6c2a5
Kak-nazvat <Kvantorium22@gmail.com>
2024-10-16 18:39:05 +0700

    Task if_else_when complited

--- src/main/kotlin/if_else_when/tasks/tasks.kt
+++ src/main/kotlin/if_else_when/tasks/tasks.kt
@@ -1,8 +1,11 @@
-package org.example.if_else_when.tasks
+
+
+
+package if_else_when.tasks
 
 
 fun main(){
-
+    task8()
 }
 
 
@@ -13,7 +16,16 @@ Task1: Чётное или нечётное число
 Определи, является ли число чётным или нечётным, и выведи соответствующее сообщение.
 */
 fun task1() {
+    println("Введите число:")
+    val number = readLine()!!.toInt()
+
+    if (number % 2 == 0) {
+        println("$number - четное число.")
+    } else {
+        println("$number - нечетное число.")
+    }
 }
+
 
 /*
 Task2: Определение возраста
@@ -23,6 +35,16 @@ Task2: Определение возраста
 Если от 18 до 65, выведи "Вы взрослый". Если больше 65, выведи "Вы пенсионер".
 */
 fun task2() {
+    println("Введите свой возраст:")
+    val age = readLine()!!.toInt()
+
+    if (age < 18) {
+        println("Вы ещё подросток")
+    } else if (age in 18..65) {
+        println("Вы взрослый")
+    } else {
+        println("Вы пенсионер")
+    }
 }
 
 /*
@@ -33,8 +55,18 @@ Task3: Калькулятор скидки
 В противном случае выведи сумму без изменений.
 */
 fun task3() {
-}
+    println("Введите сумму покупки:")
+    val purchaseAmount = readLine()!!.toDouble()
 
+    if (purchaseAmount > 1000) {
+        val discount = purchaseAmount * 0.1
+        val finalAmount = purchaseAmount - discount
+        println("Скидка 10%: $discount")
+        println("Итоговая сумма: $finalAmount")
+    } else {
+        println("Итоговая сумма: $purchaseAmount")
+    }
+}
 /*
 Task4: Максимальное из двух чисел
 
@@ -42,6 +74,14 @@ Task4: Максимальное из двух чисел
 Определи и выведи на экран большее из них.
 */
 fun task4() {
+    println("Введите первое число:")
+    val num1 = readLine()!!.toInt()
+
+    println("Введите второе число:")
+    val num2 = readLine()!!.toInt()
+
+    val maxNumber = if (num1 > num2) num1 else num2
+    println("Максимальное число: $maxNumber")
 }
 
 /*
@@ -50,8 +90,22 @@ Task5: Игра "Угадай число"
 Задание: Сгенерируй случайное число от 1 до 5.
 Попроси пользователя угадать число. Если он угадал, выведи "Поздравляем, вы угадали!", иначе выведи "Неправильно, попробуйте ещё раз".
 */
+
 fun task5() {
+    val secretNumber = (1..5).random() // Генерируем случайное число от 1 до 5
+    println("Я загадал число от 1 до 5. Попробуйте угадать:")
+
+    while (true) {
+        val guess = readLine()!!.toInt()
+        if (guess == secretNumber) {
+            println("Поздравляем, вы угадали!")
+            break
+        } else {
+            println("Неправильно, попробуйте ещё раз.")
+        }
+    }
 }
+
 
 /*
 Task6: Проверка пароля
@@ -60,6 +114,16 @@ Task6: Проверка пароля
 Попроси пользователя ввести пароль. Если введённый пароль совпадает, выведи "Доступ разрешен", иначе "Доступ запрещен".
 */
 fun task6() {
+    val password = "1234" // Заданный пароль
+
+    println("Введите пароль:")
+    val inputPassword = readLine()!!
+
+    if (inputPassword == password) {
+        println("Доступ разрешен")
+    } else {
+        println("Доступ запрещен")
+    }
 }
 
 /*
@@ -69,6 +133,14 @@ Task7: Определение знака числа
 Определи, положительное оно, отрицательное или равно нулю, и выведи соответствующее сообщение.
 */
 fun task7() {
+    println("Введите число:")
+    val number = readLine()!!.toInt()
+
+    when {
+        number > 0 -> println("Число положительное")
+        number < 0 -> println("Число отрицательное")
+        else -> println("Число равно нулю")
+    }
 }
 
 /*
@@ -78,7 +150,32 @@ Task8: Калькулятор простых операций
 Выполни указанную операцию и выведи результат.
 */
 fun task8() {
+    println("Введите первое число:")
+    val num1 = readLine()!!.toInt()
 
+    println("Введите второе число:")
+    val num2 = readLine()!!.toInt()
+
+    println("Введите операцию (+, -, *, /):")
+    val operation = readLine()!!
+
+    val result = when (operation) {
+        "+" -> num1 + num2
+        "-" -> num1 - num2
+        "*" -> num1 * num2
+        "/" -> if (num2 == 0) {
+            println("Деление на ноль невозможно!")
+            Double.NaN // Возвращаем NaN (Not a Number) в случае деления на ноль
+        } else {
+            num1 / num2
+        }
+        else -> {
+            println("Некорректная операция!")
+            Double.NaN
+        }
+    }
+
+    println("Результат: $result")
 }
 
 /*
@@ -88,7 +185,24 @@ Task9: Определение високосного года
 Определи, является ли этот год високосным, и выведи результат.
 */
 fun task9() {
+    println("Введите год:")
+    val year = readLine()!!.toInt()
 
+    val isLeapYear = if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            year % 400 == 0
+        } else {
+            true
+        }
+    } else {
+        false
+    }
+
+    if (isLeapYear) {
+        println("$year - високосный год")
+    } else {
+        println("$year - не високосный год")
+    }
 }
 
 /*
@@ -98,4 +212,16 @@ Task10: Определение буквы
 Определи, является ли она гласной или согласной, и выведи соответствующее сообщение.
 */
 fun task10() {
+    println("Введите букву:")
+    val letter = readLine()!!.toLowerCase()
+
+    if (letter.length != 1) {
+        println("Введите одну букву!")
+        return
+    }
+
+    when (letter) {
+        "a", "e", "i", "o", "u","y" -> println("$letter - гласная буква")
+        else -> println("$letter - согласная буква")
+    }
 }
