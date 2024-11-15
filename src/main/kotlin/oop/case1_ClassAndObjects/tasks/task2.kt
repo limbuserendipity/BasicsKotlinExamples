0807bb1a7995858285753d14c9222e13098b2ca8
User <you@example.com>
2024-11-15 11:30:53 +0700

    classes task2.kt completed

--- src/main/kotlin/oop/case1_ClassAndObjects/tasks/task2.kt
+++ src/main/kotlin/oop/case1_ClassAndObjects/tasks/task2.kt
@@ -10,3 +10,71 @@ package org.example.oop.case1_ClassAndObjects.tasks
     findBooksByAuthor(author: String) — возвращает список книг указанного автора.
 
  */
+fun main() {
+    val book = Book(
+        title = "растения против зомби",
+        author = "мартышка хамстер 1490",
+        yearPublished = 1988
+    )
+
+    val book2 = Book(
+        title = "Растения против хамстеров",
+        author = "дима228_хакер",
+        yearPublished = -960
+    )
+
+    val book3 = Book(
+        title = "Scratch vs Roblox",
+        author = "Лев Толстый",
+        yearPublished = 2025
+    )
+
+    val book4 = Book(
+        title = "СОЮЗ НЕРУШИМЫЙ 1⃣️⃣8⃣8",
+        author = "Лев Толстый",
+        yearPublished = 52
+    )
+
+    val library = Library()
+    library.addBook(book)
+    library.addBook(book2)
+    library.addBook(book3)
+    library.addBook(book4)
+
+//    library.listBooks()
+    library.findBookByAuthor("Лев Толстый")
+}
+
+class Book(
+    val title: String,
+    val author: String,
+    val yearPublished: Int
+)
+
+class Library() {
+    val books: MutableList<Book> = mutableListOf()
+
+    fun addBook(book: Book) {
+        books.add(book)
+    }
+
+    fun listBooks() {
+        books.forEach { book_ ->
+            println("💫 Название: ${book_.title}")
+            println("🎨 Автор: ${book_.author}")
+            println("1⃣💀☠  Год: ${book_.yearPublished}")
+            println()
+        }
+    }
+
+    fun findBookByAuthor(author: String) {
+        books.forEach { book_ ->
+            if (book_.author == author) {
+                println("💫 Название: ${book_.title}")
+                println("🎨 Автор: ${book_.author}")
+                println("1⃣💀☠  Год: ${book_.yearPublished}")
+                println()
+            }
+        }
+    }
+}
